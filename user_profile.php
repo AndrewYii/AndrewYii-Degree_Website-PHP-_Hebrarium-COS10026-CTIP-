5b2de995fe9d3437cd7122f78e56b5de70b54c3c
keaniqan <aniqnazhan1234@gmail.com>
2024-11-13 13:17:05 +0800

    NOT FINISHED

--- user_profile.php
+++ user_profile.php
@@ -76,62 +76,123 @@
                     $plant_result = mysqli_query($conn, $plant_sql);
 
                     if ($plant_result && mysqli_num_rows($plant_result) > 0) {
-                        $contribution_count = 1;
-                            while ($row = mysqli_fetch_assoc($plant_result)) {
-                                echo "<h3>Contribution #" . $contribution_count . "</h3>";
-                                echo "<form method='POST' onsubmit='return confirm(\"Are you sure you want to delete this contribution?\");'>";
-                                echo "<input type='hidden' name='contribution_id' value='" . $row['Contribute_ID'] . "'>";
-                                echo "<button type='submit' name='delete_contribution' class='delete-btn'>Delete Contribution</button>";
-                                echo "</form>";
-                                echo "<table border='1'>
-                                <tr>
-                                    <th>Contribution</th>   
-                                    <th>Details</th>
-                                </tr>
-                                <tr>
-                                    <td>Plant's Leaf</td>
-                                    <td><img src='" . htmlspecialchars($row['Plant_Leaf_Photo']) . "' alt='Plant Leaf Photo' class='enquiry-img'></td>
-                                </tr>
-                                <tr>
-                                    <td>Herbarium Species</td>
-                                    <td><img src='" . htmlspecialchars($row['Plant_Herbarium_Photo']) . "' alt='Plant Herbarium Photo' class='enquiry-img'></td>
-                                </tr>
-                                <tr>
-                                    <td>Plant's Name</td>
-                                    <td>" . htmlspecialchars($row['Plant_Name']) . "</td>
-                                </tr>
-                                <tr>
-                                    <td>Plant's Family</td>
-                                    <td>" . htmlspecialchars($row['Plant_Family']) . "</td>
-                                </tr>
-                                <tr>
-                                    <td>Plant's Genus</td>
-                                    <td>" . htmlspecialchars($row['Plant_Genus']) . "</td>
-                                </tr>
-                                <tr>
-                                    <td>Plant's Species</td>
-                                    <td>" . htmlspecialchars($row['Plant_Species']) . "</td>
-                                </tr>
-                                <tr>
-                                    <td>Description</td>
-                                    <td>" . htmlspecialchars($row['Description_Contribute']) . "</td>
-                                </tr>
-                            </table>
-                            <br>";
-                                $contribution_count++;
-                            }
-                            } else {
-                            echo "<p>No plant contributions found</p>";
-                            }
-                            if (isset($_POST['delete_contribution'])) {
-                                $contribution_id = $_POST['contribution_id'];
-                                $delete_sql = "DELETE FROM contribute WHERE Contribute_ID = '$contribution_id' AND username = '$current_user'";
-                                if (mysqli_query($conn, $delete_sql)) {
-                                    echo "";
-                                } else {
-                                    echo "";
-                            }
+                    $contribution_count = 1;
+                        while ($row = mysqli_fetch_assoc($plant_result)) {
+                            echo "<h3>Contribution #" . $contribution_count . "</h3>";
+                            echo "<form method='POST' onsubmit='return confirm(\"Are you sure you want to delete this contribution?\");'>";
+                            echo "<input type='hidden' name='contribution_id' value='" . $row['Contribute_ID'] . "'>";
+                            echo "<button type='submit' name='delete_contribution' class='delete-btn'>Delete Contribution</button>";
+                            echo "</form>";
+                            echo "<table border='1'>
+                            <tr>
+                                <th>Contribution</th>   
+                                <th>Details</th>
+                            </tr>
+                            <tr>
+                                <td>Plant's Leaf</td>
+                                <td><img src='" . htmlspecialchars($row['Plant_Leaf_Photo']) . "' alt='Plant Leaf Photo' class='enquiry-img'></td>
+                            </tr>
+                            <tr>
+                                <td>Herbarium Species</td>
+                                <td><img src='" . htmlspecialchars($row['Plant_Herbarium_Photo']) . "' alt='Plant Herbarium Photo' class='enquiry-img'></td>
+                            </tr>
+                            <tr>
+                                <td>Plant's Name</td>
+                                <td>" . htmlspecialchars($row['Plant_Name']) . "</td>
+                            </tr>
+                            <tr>
+                                <td>Plant's Family</td>
+                                <td>" . htmlspecialchars($row['Plant_Family']) . "</td>
+                            </tr>
+                            <tr>
+                                <td>Plant's Genus</td>
+                                <td>" . htmlspecialchars($row['Plant_Genus']) . "</td>
+                            </tr>
+                            <tr>
+                                <td>Plant's Species</td>
+                                <td>" . htmlspecialchars($row['Plant_Species']) . "</td>
+                            </tr>
+                            <tr>
+                                <td>Description</td>
+                                <td>" . htmlspecialchars($row['Description_Contribute']) . "</td>
+                            </tr>
+                        </table>
+                        <br>";
+                            $contribution_count++;
                         }
+                        } else {
+                        echo "<p>No plant contributions found</p>";
+                    }
+                    if (isset($_POST['delete_contribution'])) {
+                        $contribution_id = $_POST['contribution_id'];
+                        $delete_sql = "DELETE FROM contribute WHERE Contribute_ID = '$contribution_id' AND username = '$current_user'";
+                        if (mysqli_query($conn, $delete_sql)) {
+                            echo "";
+                        } else {
+                                    echo "";
+                        }
+                    }
+                    
+                    $enquiry_sql = "SELECT * FROM Enquiry WHERE Username = '$current_user'";
+                    $enquiry_result = mysqli_query($conn, $enquiry_sql);
+                    
+                    if ($enquiry_result && mysqli_num_rows($enquiry_result) > 0) {
+                        $enquiry_count = 1;
+                        while ($row = mysqli_fetch_assoc($enquiry_result)) {
+                            echo "<h3>Enquiry #" . $enquiry_count . "</h3>";
+                            echo "<form method='POST'>";
+                            echo "<input type='hidden' name='enquiry_id' value='" . $row['Enquiry_ID'] . "'>";
+                            //echo "<label for='delete_enquiry' class='delete-btn'>Delete Enquiry</label>";
+                            echo "<button type='submit' name='delete_enquiry' class='delete-btn'><label for='delete_enquiry'>Delete Enquiry</label></button>";
+                            echo "<input type='checkbox' id='delete_enquiry' class='delete_enquiry_checkbox'>";
+                            echo "<div class='delete_enquiry_background'>
+                            <div class='delete_enquiry_content'>
+                                <p>Enquiry is successfully deleted</p>
+                                <a href='user_profile.php' class='confirm-logout'>Refresh Page</a>
+                            </div>
+                            </div>";
+                            echo "</form>";
+                            echo "<table border='1'>
+                            <tr>
+                                <th>Enquiry</th>   
+                                <th>Details</th>
+                            </tr>
+                            <tr>
+                                <td>Name</td>
+                                <td>" . htmlspecialchars($row['Name']) . "</td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td>" . htmlspecialchars($row['Email']) . "</td>
+                            </tr>
+                            <tr>
+                                <td>Date</td>
+                                <td>" . htmlspecialchars($row['Enquiry_Created_At']) . "</td>
+                            </tr>
+                            <tr>
+                                <td>Subject</td>
+                                <td>" . htmlspecialchars($row['Subject']) . "</td>
+                            </tr>
+                            <tr>
+                                <td>Message</td>
+                                <td>" . htmlspecialchars($row['Message']) . "</td>
+                            </tr>
+                        </table>";
+                            $enquiry_count++;   
+                        }
+                    }
+
+                    if (isset($_POST['delete_enquiry'])) {
+                        $enquiry_id = $_POST['enquiry_id'];
+                        $delete_enquiry_sql = "DELETE FROM Enquiry WHERE Enquiry_ID = '$enquiry_id' AND Username = '$current_user'";
+                        if (mysqli_query($conn, $delete_enquiry_sql)) {
+                            echo "";//TODO: Pop up message
+                        } else {
+                            echo "";//TODO: Pop up message
+                        }
+                    }
+
+                    
                 ?>
                 <br>
             </div>
